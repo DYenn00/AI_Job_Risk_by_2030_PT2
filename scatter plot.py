895358e60da3f7d2f5b275bcdd16c9b61956e396
DYenn00 <55598370+DYenn00@users.noreply.github.com>
2026-01-03 15:46:30 -0500

    Add scatter plot for Tech Growth vs Automation Probability

---
+++ scatter plot.py
@@ -0,0 +1,3 @@
+from matplotlib import pyplot as plt
+_df_10.plot(kind='scatter', x='Tech_Growth_Factor', y='Automation_Probability_2030', s=32, alpha=.8)
+plt.gca().spines[['top', 'right',]].set_visible(False)
